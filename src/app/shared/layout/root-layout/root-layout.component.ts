a6883d4a7b4ac20926b40faae921921ce381eeb2
Paritosh Jha <paritoshjha0504@gmail.com>
2024-11-18 16:29:13 +0530

    Refactor components to add root layout

---
+++ src/app/shared/layout/root-layout/root-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './root-layout.component.html',
+  styleUrl: './root-layout.component.css'
+})
+export class RootLayoutComponent {
+
+}
